dfc0ce1af18847db902c4ae47b4c32a57328dd67
shellc0de <sdown7@gmail.com>
2012-11-17 12:49:05 +0700

    Update projectpertama/projectpertama/urls.py

--- projectpertama/projectpertama/urls.py
+++ projectpertama/projectpertama/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    #url(r'^$', 'uvt.views.home', name='home'),
+    url(r'^$', 'uvt.views.home', name='home'),
     # url(r'^projectpertama/', include('projectpertama.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
